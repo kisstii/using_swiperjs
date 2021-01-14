5b32d82f2d27554b57c5b47fab38cf5d80c45b69
Tamas Kiss <kisstii1@gmail.com>
2021-01-14 09:55:34 +0100

    ready

---
+++ script.js
@@ -0,0 +1,14 @@
+function loader() {
+
+	var mySwiper = new Swiper('.swiper-container', {
+			direction: 'vertical',
+		loop: true,
+	
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		}
+	});
+
+}
+window.addEventListener("load", loader);
